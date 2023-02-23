985bbdfe3c96ec1dcae9d09cdf354fd56d9f8979
brandon <brandon.w.leung@gmail.com>
2023-02-23 15:12:47 -0800

    my transpose

--- w4d4/practice-for-ch-ruby-first-tdd-long-practice-main/tdd_project/lib/tdd.rb
+++ w4d4/practice-for-ch-ruby-first-tdd-long-practice-main/tdd_project/lib/tdd.rb
@@ -1,12 +1,26 @@
 
 def my_uniq(arr)
+    ans = []
+    (0...arr.length).each do | i | 
+        if !ans.include?(arr[i])
+            ans << arr[i]
+        end
+    end
+   ans
+end
 
-    # (0...arr.length).each do | i | 
-    #     if ans.include?arr[i]
+def two_sum(arr)
+    ans = []
+    (0...arr.length).each do |i|
+        (i + 1...arr.length).each do |j|
+            if arr[i] + arr[j] == 0
+                ans << [i, j]
+            end
+        end
+    end
+    ans
+end
 
-    #     end
-    # arr.uniq
+def my_transpose(matrix)
 
-
-   
 end
--- w4d4/practice-for-ch-ruby-first-tdd-long-practice-main/tdd_project/spec/tdd_spec.rb
+++ w4d4/practice-for-ch-ruby-first-tdd-long-practice-main/tdd_project/spec/tdd_spec.rb
@@ -7,14 +7,54 @@ describe "#uniq" do
         expect(my_uniq(arr)).to eq([1, 2, 3])
     end
     it "doesn't call Array#uniq" do
-        
         expect(arr).not_to receive(:uniq)
         my_uniq(arr)
     end
-
+    it "output is an Array" do
+        expect(my_uniq(arr).is_a?(Array)).to be true
+    end
 end
 
+describe "#two_sum" do
+    subject(:arr) {[-1, 0, 2, -2, 1]}
+    it "finds a pair of elements that add to zero" do
+        expect(two_sum([-1, 1])).to eq([[0,1]])
+    end
+    it "output is an Array" do
+        expect(two_sum(arr).is_a?(Array)).to be true
+    end
+    it "each pair is sorted with smaller index before bigger index" do
+        expect(two_sum(arr)).to eq([[0, 4], [2, 3]])
+    end
+end
 
+describe "#my_transpose" do
+    subject(:matrix) {[
+                        [0, 1, 2],
+                        [3, 4, 5],
+                        [6, 7, 8]
+                        ]}
+    let(:switched) {cols = [
+                        [0, 3, 6],
+                        [1, 4, 7],
+                        [2, 5, 8]
+                        ]}
+    it "returns a transposed square" do
+        expect(my_transpose(matrix)).to eq(switched)
+    end
+     it "output is an Array" do
+        expect(my_transpose(matrix).is_a?(Array)).to be true
+    end
+    it "doesn't call Array#transpose" do
+        expect(matrix).not_to recieve(:transpose)
+        my_transpose(matrix)
+    end
+end
+
+#  it "doesn't call Array#uniq" do
+#         expect(arr).not_to receive(:uniq)
+#         my_uniq(arr)
+#     end
 
 
 
